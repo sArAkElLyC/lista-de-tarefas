a0dad0bf9a648ff0f82403f729c198932af115f3
Sara <sarakellycardosofischer53@gmail.com>
2023-09-12 16:11:11 -0300

    Estou subindo minha primeira lista de tarefas no GitHub

---
+++ js/explicaçãosoArray.js
@@ -0,0 +1,34 @@
+//1- um array sem elementos
+let lista = [];
+
+let numeros = [1,2,4,5,7,9];
+
+let misto = [1.1, true, "SARA"];
+
+//1- chamada sem argumentos/elementos
+var a = new Array();
+
+//chamada com um único argumento númerico,
+//o qual especifica um comprimento
+//o numero é o quanto o array suporta
+var b = new Array(10);
+//geralmente é usado quando se tem uma quantidade específica programada(dias da semana, etc)
+
+//especificação explícita de dois ou mais elementos
+//de array ou de apenas um elemento não númérico para o array:
+var c = new Array(5,4,3,2,1, "testing, testing");
+
+
+//TODO MÉTODO CONSTRUTOR COMEÇA COM MAIUSCULO ASSIM COMO 'Array'
+//começa com um array de um elemento
+var a = ["world"]; //pode ser qualquer nome
+//lê o elemento 0
+var value = a[0]; 
+//grava o elemento 1
+a[1] = 3.14;
+//grava o elemento 2
+i = 2;
+a[i] = 3;
+//grava o elemento 3
+a[i+1] = "hola";
+
